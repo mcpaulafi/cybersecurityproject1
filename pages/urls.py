fc4dfe088a79821bc30db8aa5600464664206733
Paula Meuronen <paula.meuronen@helsinki.fi>
2025-08-29 17:42:22 +0300

    Flaw 4 second fix

--- pages/urls.py
+++ pages/urls.py
@@ -5,10 +5,14 @@ from django.urls import path
 
 from . import views
 
+# SECURITY FLAW 4: Identification and Authentication Failures 
+# Fix here by
+# disable this line by commenting it out
+# path("changepswd/", views.changepswd, name="changepswd"),
+
 # SECURITY FLAW 5: Security Misconfiguration
 # Fix here by
-# a) remove comments from lines in urlpatterns
-# b) and then disable this line by commenting it out
+# disable this line by commenting it out
 # path("forgot/", views.forgot, name="forgot"),
 
 urlpatterns = [
@@ -19,12 +23,12 @@ urlpatterns = [
     path("question/", views.question, name="question"),
     path("changepswd/", views.changepswd, name="changepswd"),
 
-    # path('password_reset/', auth_views.PasswordResetView.as_view(),
-    #  name='password_reset'),
-    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
-    #  name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
-    #  name='password_reset_confirm'),
-    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
-    #  name='password_reset_complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+     name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+     name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+     name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+     name='password_reset_complete'),
 ]
